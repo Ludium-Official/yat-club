daa663835fe7980e588f4ec527827e1abb8c5d96
juniahn-dev <gud0415@gmail.com>
2025-03-11 19:54:25 -0700

    [feat] Change init

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import withPWA from "next-pwa";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: isDev,
 })({
   reactStrictMode: true,
 });
